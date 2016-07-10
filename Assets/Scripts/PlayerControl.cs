5b85b5346f605e53471427fe92f586d552cf03d4
Pitri-Mac <pjoetri@gmail.com>
2016-07-10 03:43:43 +0200

    init

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	public float moveSpeed = 10;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float ver = Input.GetAxisRaw ("Vertical");
+		float hor = Input.GetAxisRaw ("Horizontal");
+			
+		if (ver != 0)
+			rb.transform.Translate(Vector3.forward * ver * Time.deltaTime);
+		if (hor != 0)
+			rb.transform.Translate(Vector3.right * hor * Time.deltaTime);
+
+		if (Input.GetKeyDown(KeyCode.Space) )
+			Handles.DrawCamera (new Rect(0,0,1024,800), Camera.allCameras [1]);
+	}
+}
